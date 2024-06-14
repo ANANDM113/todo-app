668ec25ff7327f1cd374fc408076c2f4b18a7612
Anand <anandmaheshwari113@gmail.com>
2024-06-14 19:46:36 +0530

    todo-app-react

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./styles.css";
+import { Component } from "react";
+import { Form } from "./Form";
+import {List} from "./List";
+
+export default class App extends Component{
+  
+  constructor(props){
+  
+    super(props);
+    this.state  = {
+      todos:[
+        {text:"Do the laundry"},
+        {text:"Iron the clothes"},
+        {text:"Go for a walk"}
+      ]
+    };
+  }
+
+  handleAdd = (text) =>  {
+    this.setState((prev)  =>  ({
+      todos:[{text},...prev.todos]
+    }));
+  }
+
+  handleRemove  = (text)  =>  {
+    const filteredTodos = this.state.todos.filter((todo) => todo.text!==text);
+    this.setState({todos:filteredTodos});
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <span>Todo</span>
+        <Form onAdd={this.handleAdd}/>
+        <List todos={this.state.todos} onRemove={this.handleRemove}/>
+      </div>
+    );
+  }
+
+}
